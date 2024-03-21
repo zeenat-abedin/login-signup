79f224f67ccb86f00c4ca92f0295ffcac838df10
zeenat <zeenu.23@gmail.com>
2024-03-21 11:17:12 +0530

    "Update to React Router v6: replace `Route` components with `Routes` and update syntax"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
@@ -25,6 +25,7 @@ function App() {
     <Router>
       <div className="App">
         <Header loggedIn={loggedIn} handleLogout={handleLogout} />
+        <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/signup" render={(props) => <Signup {...props} handleLogin={handleLogin} />} />
           <Route path="/login" render={(props) => <Login {...props} handleLogin={handleLogin} />} />
@@ -32,6 +33,7 @@ function App() {
             path="/protected"
             render={(props) => <ProtectedPage {...props} loggedIn={loggedIn} user={user} />}
           />
+          </Routes>
       </div>
     </Router>
   );
